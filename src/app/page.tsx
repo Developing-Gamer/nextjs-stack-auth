1b7d41a314d979e5e8d9c2a1a82630c67ee97337
Armaan Jain <armaanj@umd.edu>
2025-10-11 18:33:34 -0700

    removed header & footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,15 @@
 'use client';
 
 import { useUser } from "@stackframe/stack";
-import { Header } from "@/components/Header";
 import { AuthenticatedView } from "@/components/AuthenticatedView";
 import { UnauthenticatedView } from "@/components/UnauthenticatedView";
-import { Footer } from "@/components/Footer";
 
 export default function Home() {
   const user = useUser();
 
   return (
-    <div className="font-sans min-h-screen p-8 pb-20">
-      <Header user={user} />
-
-      <main className="flex flex-col gap-8 items-center justify-center max-w-2xl mx-auto">
-        {user ? <AuthenticatedView user={user} /> : <UnauthenticatedView />}
-
-        <Footer />
-      </main>
-    </div>
+    <>
+      {user ? <AuthenticatedView user={user} /> : <UnauthenticatedView />}
+    </>
   );
 }
